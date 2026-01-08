f8a6173e44b60d4ca20eff0e059db774bc8b8643
Jon Ballard <jon.ballard@gmail.com>
2026-01-08 14:56:27 -0600

    hyprland overrides script fix

--- hyperland-overrides/install-hyprland-overrides.sh
+++ hyperland-overrides/install-hyprland-overrides.sh
@@ -4,8 +4,7 @@ set -e
 
 HYPRLAND_CONFIG_DIR="$HOME/.config/hypr"
 HYPRLAND_CONFIG="$HYPRLAND_CONFIG_DIR/hyprland.conf"
-TWEAK_REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-OVERRIDES_DIR="$TWEAK_REPO_DIR/hyprland-overrides"
+OVERRIDES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Check if hyprland config exists
 if [ ! -f "$HYPRLAND_CONFIG" ]; then
@@ -15,16 +14,16 @@ if [ ! -f "$HYPRLAND_CONFIG" ]; then
 fi
 
 # Check if overrides config exists
-if [ ! -f "$OVERRIDES_DIR" ]; then
+if [ ! -f "$OVERRIDES_DIR/override-monitors.conf" ]; then
     echo "Overrides config dir not found at $OVERRIDES_DIR"
     exit 1
 fi
 
-./append_overrides "$HYPRLAND_CONFIG_DIR/monitors.conf" "$OVERRIDES_DIR/override-monitors.conf"
-./append_overrides "$HYPRLAND_CONFIG_DIR/input.conf" "$OVERRIDES_DIR/override-input.conf"
-./append_overrides "$HYPRLAND_CONFIG_DIR/bindings.conf" "$OVERRIDES_DIR/override-bindings.conf"
-./append_overrides "$HYPRLAND_CONFIG_DIR/looknfeel.conf" "$OVERRIDES_DIR/override-looknfeel.conf"
-./append_overrides "$HYPRLAND_CONFIG_DIR/autostart.conf" "$OVERRIDES_DIR/override-autostart.conf"
+"$OVERRIDES_DIR/append_overrides.sh" "$HYPRLAND_CONFIG_DIR/monitors.conf" "$OVERRIDES_DIR/override-monitors.conf"
+"$OVERRIDES_DIR/append_overrides.sh" "$HYPRLAND_CONFIG_DIR/input.conf" "$OVERRIDES_DIR/override-input.conf"
+"$OVERRIDES_DIR/append_overrides.sh" "$HYPRLAND_CONFIG_DIR/bindings.conf" "$OVERRIDES_DIR/override-bindings.conf"
+"$OVERRIDES_DIR/append_overrides.sh" "$HYPRLAND_CONFIG_DIR/looknfeel.conf" "$OVERRIDES_DIR/override-looknfeel.conf"
+"$OVERRIDES_DIR/append_overrides.sh" "$HYPRLAND_CONFIG_DIR/autostart.conf" "$OVERRIDES_DIR/override-autostart.conf"
 
 #--- backup and overwrite screensaver/lock settings
 mv "$HYPRLAND_CONFIG_DIR/hypridle.conf" "$HYPRLAND_CONFIG_DIR/hypridle.conf.orig"
